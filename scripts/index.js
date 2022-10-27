15ef5901b45edf048384526e8b00393ffd0581d1
Nikolay Dokukin <frostphoto@yandex.ru>
2022-10-27 22:11:54 +0300

    fixed errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let profileBtn = document.querySelector('.profile__btn');
+let profileBtn = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 
 let popupCloseBtn = document.querySelector('.popup__close-btn');
@@ -7,39 +7,31 @@ let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
 let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__job');
+let popupName = formElement.querySelector('.popup__input_profile_name');
+let popupJob = formElement.querySelector('.popup__input_profile_job');
 
 // open popup
-function popupOpen() {
+function openPopup() {
   popup.classList.add('popup_opened');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
+  popupName.value = profileName.textContent;
+  popupJob.value = profileJob.textContent;
 }
-
-profileBtn.addEventListener('click', popupOpen);
 
 //close popup
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-popupCloseBtn.addEventListener('click', closePopup);
-
-window.onclick = function (close) {
-  if (close.target == popup) {
-    popup.classList.remove('popup_opened');
-  }
-}
-
 // add name and job
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  profileName.textContent = popupName.value;
+  profileJob.textContent = popupJob.value;
+  closePopup();
 }
 
+profileBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 
