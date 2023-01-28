8ee51b63aeac3025660113a9a9a80bdc0b24379b
Nikolay Dokukin <frostphoto@yandex.ru>
2023-01-28 16:31:46 +0300

    fixed errors in the Card class

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,6 +4,9 @@ export default class Card {
     this._link = dataCard.link;
     this._handleCardClick = handleCardClick;
     this._templateSelector = cardTemplateSelector;
+    this._newCard = this._getTemplate();
+    this._cardImage = this._newCard.querySelector('.cards__image');
+    this._cardLike = this._newCard.querySelector('.cards__likes-btn');
   }
 
   _getTemplate() {
@@ -21,27 +24,21 @@ export default class Card {
   }
 
   _checkLike() {
-    this._newCard.querySelector('.cards__likes-btn').classList.toggle('cards__likes-btn_active');
+    this._cardLike.classList.toggle('cards__likes-btn_active');
   }
 
   _setEventListener() {
     const cardDeleteBtn = this._newCard.querySelector('.cards__delete-btn');
     cardDeleteBtn.addEventListener('click', () => { this._deleteCard() });
-
-    const cardLike = this._newCard.querySelector('.cards__likes-btn');
-    cardLike.addEventListener('click', () => { this._checkLike() });
-
-    const cardLink = this._newCard.querySelector('.cards__image');
-    cardLink.addEventListener('click', () => { this._handleCardClick(cardLink) });
+    this._cardLike.addEventListener('click', () => { this._checkLike() });
+    this._cardImage.addEventListener('click', () => { this._handleCardClick(this._cardImage) });
   }
 
   getView() {
-    this._newCard = this._getTemplate();
     this._setEventListener();
 
     this._cardName = this._newCard.querySelector('.cards__title');
     this._cardName.textContent = this._name;
-    this._cardImage = this._newCard.querySelector('.cards__image');
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
 
